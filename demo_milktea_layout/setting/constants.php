ba488fba384ffce9ac3b67399c8290a39e510ac4
Anh-Dnt <dntanh2505@gmail.com>
2024-02-24 00:26:51 +0700

    add constants

---
+++ demo_milktea_layout/setting/constants.php
@@ -0,0 +1,18 @@
+<?php
+// tên site
+define('SITE_NAME', 'Bubble Tea House');
+
+// đường dẫn thư mục
+define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/milktea');
+define('SQL_DIR', ROOT . "/sql");
+define('SETTING_DIR', ROOT . "/setting");
+
+// file config
+define("CONFIG_FILE", "config.ini");
+
+// encoding
+define("CHARSET_UTF8", "UFT-8");
+define("ENCODING_BASE64", "base64");
+
+// mail
+define("VERIFY_MAIL_SUBJECT", "Xác thực địa chỉ email của bạn");
