f4bdb6b05639f48430ee6ea410a248a657630436
Anh-Dnt <dntanh2505@gmail.com>
2024-02-27 00:38:40 +0700

    add message.js

---
+++ demo_milktea_layout/scripts/common/message.js
@@ -0,0 +1,17 @@
+const SIGNUP_MESSAGES = {
+  INF_1000: "Vui lòng nhập tên đăng nhập",
+  INF_1001: "Số ký tự được phép nhập ít nhất: {0}, nhiều nhất {1}",
+  INF_1002: "Vui lòng nhập địa chỉ email",
+  INF_1003: "Vui lòng nhập đúng format của email",
+  INF_1004: "Bạn đã nhập quá {0} ký tự!",
+  INF_1005: "Vui lòng nhập mật khẩu",
+  INF_1006: "Vui lòng nhập xác nhận mật khẩu",
+  INF_1007: "Không khớp với mật khẩu",
+};
+
+const LOGIN_MESSAGE = {
+  INF_1008: "Vui lòng nhập tên đăng nhập",
+  INF_1009: "Vui lòng nhập mật khẩu",
+};
+
+module.exports = { SIGNUP_MESSAGES };
